e0a683b462ae38d75811a33fdd8056af84c27982
Wilfredo Rujel <66637170+wrujel@users.noreply.github.com>
2022-04-01 18:39:09 -0500

    [MAR-50]: Adding route protection with login

---
+++ app/src/components/loader.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./loader.css";
+
+const Loader = () => {
+  const loadingImg = "https://cdn.auth0.com/blog/hello-auth0/loader.svg";
+
+  return (
+    <div className="container container-loader">
+      <div className="loader">
+        <img src={loadingImg} alt="Loading..." />
+      </div>
+    </div>
+  );
+};
+
+export default Loader;
--- app/src/index.js
+++ app/src/index.js
@@ -4,9 +4,12 @@ import "./index.css";
 import Home from "./pages/Home";
 import CreateEvent from "./pages/CreateEvent";
 import EditEvent from "./pages/EditEvent";
+import Profile from "./pages/Profile";
+import ProtectedRoute from "./components/ProtectedRoute";
 import Navigation from "./components/Navigation";
+import NotFound from "./pages/NotFound";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Auth0Provider } from "@auth0/auth0-react";
 
 const domain = process.env.REACT_APP_AUTH0_DOMAIN;
@@ -18,15 +21,26 @@ ReactDOM.render(
     clientId={clientId}
     redirectUri={window.location.origin}
   >
-    <Router>
+    <BrowserRouter>
       <Navigation />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
 
-        <Route path="/event/create" element={<CreateEvent />} />
-        <Route path="/event/edit/:id" element={<EditEvent />} />
+        <Route
+          path="/event/create"
+          element={<ProtectedRoute component={CreateEvent} />}
+        />
+        <Route
+          path="/event/edit/:id"
+          element={<ProtectedRoute component={EditEvent} />}
+        />
+        <Route
+          path="/profile"
+          element={<ProtectedRoute component={Profile} />}
+        />
       </Routes>
-    </Router>
+    </BrowserRouter>
   </Auth0Provider>,
   document.getElementById("root")
 );
